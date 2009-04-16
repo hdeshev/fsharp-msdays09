75babbe2c8dd0e6adca284449e52a23e6c7bdd12
Hristo Deshev <hristo.deshev@gmail.com>
2009-04-17 00:48:38 +0300

    minor fixes to the OOP demo

--- ObjectOriented/Program.fs
+++ ObjectOriented/Program.fs
@@ -3,26 +3,26 @@
 open System.Windows.Forms
 
 type IDialog =
-    abstract Show: unit -> unit
-
+    abstract Show : unit -> unit
+    
 type ButtonForm =
     inherit Form
-            
-    member this.AddButton() =
-        let button = new Button()
-        button.Text <- "Click"
-        button.Click.Add (fun e -> MessageBox.Show("click!") |> ignore )
-        button.Dock <- DockStyle.Fill
-        this.Controls.Add(button)
     
-    new() as this = {}  then
+    new() as this = {} then
         this.AddButton()
         this.Text <- "Hello, F#"
-         
+        
+    member this.AddButton() =
+        let button = new Button()
+        button.Text <- "Click me"
+        button.Click.Add (fun args -> MessageBox.Show("click!") |> ignore)
+        button.Dock <- DockStyle.Fill
+        this.Controls.Add(button)
+        
     interface IDialog with
         member this.Show() =
             this.ShowDialog() |> ignore
-    
-        
+            
+            
 let f = new ButtonForm() :> IDialog
-f.Show()
+f.Show()    
