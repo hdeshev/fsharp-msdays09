de42e2f254fe6f4c9be1a915623a47ce3bb751a1
Hristo Deshev <hristo.deshev@gmail.com>
2009-04-16 23:48:10 +0300

    Added functions demos

---
+++ Functions/Program.fs
@@ -0,0 +1,39 @@
+﻿#light
+
+let sumThree x y z =
+    x + y + z
+    
+let sumTwo5 = sumThree 5
+let sumOne10 = sumTwo5 5
+let result = sumOne10 5
+printfn "%d" result
+
+let doubled = List.map (fun x -> x * 2) [1; 2; 3]
+printfn "%A" doubled
+
+let doubledGreaterThan3 = 
+    [1; 2; 3] 
+        |> List.map (fun x -> x * 2)
+        |> List.filter (fun x -> x > 3) 
+        
+printfn "%A" doubledGreaterThan3
+
+let rec sum x =
+    if x = 0 then
+        0
+    else
+        x + sum (x - 1)
+        
+printfn "%A" (List.map sum [0; 1; 2; 3; 4; 5; 6])
+
+//printfn "%d" (sum 100000)
+
+let sumTail x =
+    let rec sumAccumulator accumulator x =
+        if x = 0 then
+            accumulator
+        else
+            sumAccumulator (accumulator + x) (x - 1)
+    sumAccumulator 0 x
+            
+printfn "%d" (sumTail 100000)
