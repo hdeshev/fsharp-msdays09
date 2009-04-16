9aa70ef592a4633b6772caf786e0fdbe265c2732
Hristo Deshev <hristo.deshev@gmail.com>
2009-04-17 00:38:42 +0300

    added the OOP demo -- WinForms baby!

---
+++ ObjectOriented/Program.fs
@@ -0,0 +1,28 @@
+﻿#light
+
+open System.Windows.Forms
+
+type IDialog =
+    abstract Show: unit -> unit
+
+type ButtonForm =
+    inherit Form
+            
+    member this.AddButton() =
+        let button = new Button()
+        button.Text <- "Click"
+        button.Click.Add (fun e -> MessageBox.Show("click!") |> ignore )
+        button.Dock <- DockStyle.Fill
+        this.Controls.Add(button)
+    
+    new() as this = {}  then
+        this.AddButton()
+        this.Text <- "Hello, F#"
+         
+    interface IDialog with
+        member this.Show() =
+            this.ShowDialog() |> ignore
+    
+        
+let f = new ButtonForm() :> IDialog
+f.Show()
